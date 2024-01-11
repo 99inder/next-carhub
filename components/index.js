afd4dc153b1f1a92aefa1cb11bb9795bbc7f9d98
Inderpal Singh <92572473+99inder@users.noreply.github.com>
2024-01-11 16:31:05 +0530

    Initial Commit

---
+++ components/index.js
@@ -0,0 +1,19 @@
+import CarCard from "./CarCard.jsx";
+import CustomButton from "./CustomButton.jsx";
+import CustomFilter from "./CustomFilter.jsx";
+import Footer from "./Footer.jsx";
+import NavBar from "./Navbar.jsx";
+import ShowMore from "./ShowMore.jsx";
+import SearchBar from "./Searchbar.jsx";
+import Hero from "./Hero.jsx";
+
+export {
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+};
